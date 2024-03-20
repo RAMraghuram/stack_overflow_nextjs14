058bd332a46ca234f79e376d30e2a4849cf3a4d2
RAMraghruam <ram309591@gmail.com>
2024-03-20 15:50:22 -0500

    add assets and layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,13 @@ export const metadata: Metadata = {
   icons: "/assets/images/site-logo.svg",
 };
 
-const inter: Inter = Inter({
+const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
 
-const spaceGrotesk: Space_Grotesk = Space_Grotesk({
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-space-grotesk",
